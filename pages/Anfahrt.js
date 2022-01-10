0f0aeb4d02582c08ac3db03b06b62e880ce8833d
tuvoljus tuvoljus <alexander.keil1@web.de>
2022-01-10 16:03:59 +0100

    Update Anfahrt 10.01.22 16:00

--- pages/Anfahrt.js
+++ pages/Anfahrt.js
@@ -8,11 +8,75 @@ import Col from "react-bootstrap/Col";
 import { ColorFont, vlAnfahrt, heading } from "../scss/styles.module.scss";
 import HeaderPicture from "../components/HeaderPicture";
 
+import { useAmp } from "next/amp";
+export const config = { amp: "hybrid" };
+
 const Anfahrt = () => {
+  const isAmp = useAmp();
   return (
     <>
       <HeaderPicture text="Meine Praxis in München" />
-      <section style={{ paddingBottom: "0px" }}>
+      <Container className={ColorFont}>
+        <Row>
+          <Col md={2}>
+            <section style={{ paddingBottom: "1rem" }}>
+              <h4>Praxis</h4>
+            </section>
+            <section>
+              <h4>Anreise</h4>
+            </section>
+          </Col>
+          <Col md={1}>
+            <section>
+              <div className={`col-md-1 d-none d-sm-block ${vlAnfahrt}`}></div>
+            </section>
+          </Col>
+          <Col md={4}>
+            <section style={{ paddingBottom: "0px" }}>
+              <div className="text-center" style={{ fontSize: "1.2em" }}>
+                <p>
+                  VITAS - Institut für Entspannungstechniken <br />
+                  Oberanger 42 | Nähe Sendlinger Tor <br />
+                  80331 München
+                </p>
+              </div>
+            </section>
+            <section style={{ paddingTop: "3rem" }}>
+              <div
+                className="text-center"
+                style={{ fontSize: "1.2em" }}
+              >
+                <p>Haltestelle Sendlinger<br/> Tor U1 / U2 / U3 / U6 / U7 / U8</p>
+                <p>Straßenbahn <br/>16 / 17 / 18 / 27 / 28</p>
+                <p>Bus 52 / 62</p>
+              </div>
+            </section>
+          </Col>
+          <Col md={5}>
+            <section>
+              {isAmp ? (
+                <amp-img
+                  layout="responsive"
+                  width="1129"
+                  height="768"
+                  src="/img_avif/Anfahrt_Bernhard_Käser.avif"
+                  alt="Anfahrt Bernhard Käser Physiopraxis"
+                />
+              ) : (
+                <Image
+                  src="/img_avif/Anfahrt_Bernhard_Käser.avif"
+                  layout="responsive"                  
+                  quality="100"
+                  alt="Anfahrt Bernhard Käser Physiopraxis"
+                  width={1129}
+                  height={768}
+                />
+              )}
+            </section>
+          </Col>
+        </Row>
+      </Container>
+      {/* <section style={{ paddingBottom: "0px" }}>
         <div className={ColorFont}>
           <div className="container text-center" style={{ marginTop: "3rem" }}>
             <div className="row">
@@ -50,7 +114,7 @@ const Anfahrt = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </>
   );
 };
