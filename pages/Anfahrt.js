a44a01f84e27dc69b89303292556fb16f6f5db61
tuvoljus tuvoljus <alexander.keil1@web.de>
2022-01-11 13:36:22 +0100

    add html lang

--- pages/Anfahrt.js
+++ pages/Anfahrt.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Head from 'next/head'
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
