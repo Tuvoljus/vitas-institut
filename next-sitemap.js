9f51870c7d5b77cfdb5b8e243d76c072d5530515
tuvoljus tuvoljus <alexander.keil1@web.de>
2022-01-11 14:38:33 +0100

    sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,30 @@
 module.exports = {
     siteUrl: 'https://www.vitas-institut.de',
     generateRobotsTxt: true,
+    autoLastmod: true,
+    transform: async (config, path) => {
+      // custom function to ignore the path
+      if (customIgnoreFunction(path)) {
+        return null
+      }
+  
+      // only create changefreq along with path
+      // returning partial properties will result in generation of XML field with only returned values.
+      if (customLimitedField(path)) {
+        // This returns `path` & `changefreq`. Hence it will result in the generation of XML field with `path` and  `changefreq` properties only.
+        return {
+          loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+          changefreq: 'weekly',
+        }
+      }
+  
+      // Use default transformation for all other cases
+      return {
+        loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+        changefreq: config.changefreq,
+        priority: config.priority,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+      }
+    },
   }
