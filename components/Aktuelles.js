d7b15111704fade0c0b306f8eb7931c3af4acc1c
tuvoljus tuvoljus <alexander.keil1@web.de>
2022-02-02 12:08:29 +0100

    update Aktuelles 02.02.2022 12:08

---
+++ components/Aktuelles.js
@@ -0,0 +1,15 @@
+import SbEditable from 'storyblok-react'
+
+const Aktuelles = ({blok}) => {
+    return (
+        <>
+        <SbEditable content={blok}>
+            <div>
+                <h2>{blok.name}</h2>
+            </div>
+        </SbEditable>
+        </>        
+    )
+}
+
+export default Aktuelles
--- pages/aktuelles_und_links.js
+++ pages/aktuelles_und_links.js
@@ -4,8 +4,9 @@ import Container  from "react-bootstrap/Container"
 import Row  from "react-bootstrap/Row"
 import Col  from "react-bootstrap/Col"
 
-import { vl, heading, inhaltBehandlung} from '../scss/styles.module.scss'
+import { vl, heading, ColorFont, inhaltBehandlung} from '../scss/styles.module.scss'
 import { Accordion } from 'react-bootstrap'
+import Aktuelles from '../components/Aktuelles'
 
 
 const AktuellesUndLinks = () => {
@@ -35,13 +36,20 @@ const AktuellesUndLinks = () => {
             <Container>
                 <Row>
                     <Col md={4} className="d-none d-sm-block">
-                        
+                        <h2 className={ColorFont}>Aktuelles</h2>
                     </Col>
                     <Col md={1} className={`d-none d-sm-block ${vl}`} style={{ color: "black" }}>
     
                     </Col>
                     <Col md={7} >
-    
+                        {/* <Aktuelles /> */}
+                        <span style={{fontWeight: '700'}}>Liebe Seitenbesucher,</span>
+                        <br /><br />
+                        seit Januar 2022 freue mich auf nach einem Re-Design meiner 10 Jahre alten Internetseite auf meine neue Interneseite Vitas Institut.
+                        <br />
+                        <br />
+                        Herzlichst,<br />
+                        Bernhard K&auml;ser
                     </Col>  
                 </Row>
             </Container>
