050aac6d41a1f00671df7cd7f60a10b8b1b661de
tuvoljus tuvoljus <alexander.keil1@web.de>
2022-02-08 10:19:06 +0100

    sitemap 08.02.2020 10:19

---
+++ components/Page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DynamicComponent from "./DynamicComponent";
+ 
+const Page = ({ blok }) => (
+  <main>
+    {blok.body
+      ? blok.body.map((blok) => (
+          <DynamicComponent blok={blok} key={blok._uid} />
+        ))
+      : null}
+  </main>
+);
+ 
+export default Page;
---
+++ components/Teaser.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { sbEditable } from "@storyblok/storyblok-editable";
+ 
+const Teaser = ({ blok }) => {
+  return   <>
+  <h2 {...sbEditable(blok)}>{blok.headline}
+  
+  </h2>
+  </>
+  
+};
+ 
+export default Teaser;
