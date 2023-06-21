a65cce46ad0a2d4b0a7d1f668847a67f755ef7d1
Alexander <alexander.keil1@web.de>
2023-06-21 20:21:57 +0200

    parkmöglichkeit

--- pages/Anfahrt.js
+++ pages/Anfahrt.js
@@ -1,30 +1,36 @@
-import React from "react";
-import Image from "next/image";
-import Head from 'next/head'
+import React from 'react';
+import Image from 'next/image';
+import Head from 'next/head';
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-import { ColorFont, vlAnfahrt, heading } from "../scss/styles.module.scss";
-import HeaderPicture from "../components/HeaderPicture";
+import { ColorFont, vlAnfahrt, heading } from '../scss/styles.module.scss';
+import HeaderPicture from '../components/HeaderPicture';
 
-import { useAmp } from "next/amp";
-export const config = { amp: "hybrid" };
+import { useAmp } from 'next/amp';
+export const config = { amp: 'hybrid' };
 
 const Anfahrt = () => {
   const headlines = {
-    fontWeight: "600",
-  }
+    fontWeight: '600',
+  };
 
   const isAmp = useAmp();
   return (
     <>
       <Head>
-      <title>Anfahrt Physiotherapie Praxis</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Bernhard Käser Physiotherapeut in München Altstadt Lehel nähe Sendlinger Tor" />
-        <meta name="keywords" content="Physiotherapeut Altstadt Lehel Sendlinger Tor" />
+        <title>Anfahrt Physiotherapie Praxis</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Bernhard Käser Physiotherapeut in München Altstadt Lehel nähe Sendlinger Tor"
+        />
+        <meta
+          name="keywords"
+          content="Physiotherapeut Altstadt Lehel Sendlinger Tor"
+        />
       </Head>
       <HeaderPicture text="Meine Praxis in München" />
       <Container className={ColorFont}>
@@ -41,9 +47,12 @@ const Anfahrt = () => {
                   ></div>
                 </Col>
                 <Col>
-                  <div className="text-center" style={{ fontSize: "1.2em" }}>
+                  <div className="text-center" style={{ fontSize: '1.2em' }}>
                     <p>
-                      <span style={headlines}>VITAS - Institut für Entspannungstechniken</span> <br />
+                      <span style={headlines}>
+                        VITAS - Institut für Entspannungstechniken
+                      </span>{' '}
+                      <br />
                       Oberanger 42 | Nähe Sendlinger Tor <br />
                       80331 München
                     </p>
@@ -60,16 +69,46 @@ const Anfahrt = () => {
                   ></div>
                 </Col>
                 <Col>
-                  <div className="text-center" style={{ fontSize: "1.2em" }}>
+                  <div className="text-center" style={{ fontSize: '1.2em' }}>
                     <p>
-                    <span style={headlines}>Haltestelle Sendlinger Tor </span>
+                      <span style={headlines}>Haltestelle Sendlinger Tor </span>
                       <br /> U1 / U2 / U3 / U6 / U7 / U8
                     </p>
                     <p>
-                    <span style={headlines}>Straßenbahn</span> <br />
+                      <span style={headlines}>Straßenbahn</span> <br />
                       16 / 17 / 18 / 27 / 28
                     </p>
-                    <p><span style={headlines}>Bus</span> <br /> 52 / 62</p>
+                    <p>
+                      <span style={headlines}>Bus</span> <br /> 52 / 62
+                    </p>
+                  </div>
+                </Col>
+              </Row>
+              <br />
+              <Row>
+                <Col md={2}>
+                  <h4>Auto</h4>
+                </Col>
+                <Col md={1}>
+                  <div
+                    className={`col-md-1 d-none d-sm-block ${vlAnfahrt}`}
+                  ></div>
+                </Col>
+                <Col md={9}>
+                  <div
+                    className="text-center"
+                    style={{ fontSize: '1.2em', listStyle: 'none' }}
+                  >
+                    <p>
+                      <span style={headlines}>Parkmöglichkeit:</span>
+                    </p>
+
+                    <li>
+                      - Contipark Tiefgarage Oberanger 27
+                      <br /> oder
+                    </li>
+
+                    <li>- Parkplätze vor dem Haus gebührenpflichtig</li>
                   </div>
                 </Col>
               </Row>
